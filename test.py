26118431df4d379723381574f166e56d012e0968
Marc Wang <474733787@qq.com>
2018-05-04 15:20:20 +0800

    lr

--- test.py
+++ test.py
@@ -250,8 +250,8 @@ eng_prefixes = (
 
 def filterPair(p):
     return len(p[0].split(' ')) < MAX_LENGTH and \
-        len(p[1].split(' ')) < MAX_LENGTH and \
-        p[1].startswith(eng_prefixes)
+        len(p[1].split(' ')) < MAX_LENGTH
+           # and \p[1].startswith(eng_prefixes)
 
 
 def filterPairs(pairs):
