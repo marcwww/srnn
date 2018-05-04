00f5d22cfd8f3822ccc1b4e98d72448670291549
Marc Wang <474733787@qq.com>
2018-05-04 15:15:19 +0800

    lr

--- test.py
+++ test.py
@@ -95,6 +95,7 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ######################################################################
